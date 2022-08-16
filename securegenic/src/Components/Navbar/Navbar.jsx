47087b39771ca5e526bc40c5f6661b25606eafe9
unknown <osamaaslam029@gmail.com>
2022-08-16 17:14:03 +0500

    hameburger

--- securegenic/src/Components/Navbar/Navbar.jsx
+++ securegenic/src/Components/Navbar/Navbar.jsx
@@ -2,10 +2,69 @@ import React from 'react'
 import './Navbar.scss'
 import n_logo from '../../Assets/Navbar/logo.png'
 import n_button from '../../Assets/Navbar/button.svg'
-
-
+import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/Box';
+import SwipeableDrawer from '@mui/material/SwipeableDrawer';
+import CloseIcon from '@mui/icons-material/Close';
+import { Link } from 'react-router-dom';
+import dash from '../../Assets/Navbar/dashboard.png'
+import wifi from '../../Assets/Navbar/wifi_profile.png'
+import threshold from '../../Assets/Navbar/threshold.png'
+import privacy from '../../Assets/Navbar/privacy_policy.png'
+import locataion from '../../Assets/Navbar/location.png'
+import groups from '../../Assets/Navbar/groups.png'
+import enroll from '../../Assets/Navbar/enroll_devices.png'
+import devices from '../../Assets/Navbar/devices.png'
+import devicehealth from '../../Assets/Navbar/deviceheath.png'
+import deep from '../../Assets/Navbar/deepdive.png'
+import administration from '../../Assets/Navbar/administration.png'
+import battery from '../../Assets/Navbar/battery_report.png'
+import apply from '../../Assets/Navbar/application_management.png'
 
 const Navbar = () => {
+    const [state, setState] = React.useState({
+
+        right: false,
+    });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (
+            event &&
+            event.type === 'keydown' &&
+            (event.key === 'Tab' || event.key === 'Shift')
+        ) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+        <Box
+            sx={{ width: anchor === 'top' || anchor === 'bottom' ? 'auto' : 250 }}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <div className='main'>
+                <div className="close"><CloseIcon/></div>
+                <div className="nav"><img src={dash} alt="" /><Link to="/" >Dashboard</Link></div>
+                <div className="nav"><img src={deep} alt="" /><Link to="/DeepDive" >DeepDive</Link></div>
+                <div className="nav"><img src={enroll} alt="" /><Link to="/EnrollDevices" >Enroll Devices</Link></div>
+                <div className="nav"><img src={devices} alt="" /><Link to="/Device" >Devices </Link></div>
+                <div className="nav"><img src={apply} alt="" /><Link to="/Application" > Application Management</Link></div>
+                <div className="nav"><img src={wifi} alt="" /><Link to="/Wifi" >Wifi Profile</Link></div>
+                <div className="nav"><img src={groups} alt="" /><Link to="/Group" >Group</Link></div>
+                <div className="nav"><img src={locataion} alt="" /><Link to="/Location" >Location</Link></div>
+                <div className="nav"><img src={threshold} alt="" /><Link to="/Utilities" >Utilities</Link></div>
+                <div className="nav"><img src={administration} alt="" /><Link to="/Administration" >Administration</Link></div>
+                <div className="nav"><img src={devicehealth} alt="" /><Link to="/DeviceHealth" >Device Health Report</Link></div>
+                <div className="nav"><img src={battery} alt="" /><Link to="/Reports" > Reports</Link></div>
+                <div className="nav"><img src={privacy} alt="" /><Link to="/PrivacyPolicy" >Privacy Policy</Link></div>
+            </div>
+
+        </Box>
+    );
     return (
         <>
             <div className="navbar">
@@ -16,9 +75,30 @@ const Navbar = () => {
                     <span>Welcome, admin</span>
                     <img src={n_button} alt="" />
                 </div>
-            </div>
+           
+            
+            <div className="hameburger">
+               <div className='ham_none'>
+                    {['right'].map((anchor) => (
+                        <React.Fragment key={anchor}>
 
-           </>
+                            <div className="menu">
+                                <MenuIcon onClick={toggleDrawer(anchor, true)} />
+                            </div>
+                            <SwipeableDrawer
+                                anchor={anchor}
+                                open={state[anchor]}
+                                onClose={toggleDrawer(anchor, false)}
+                                onOpen={toggleDrawer(anchor, true)}
+                                >
+                                {list(anchor)}
+                            </SwipeableDrawer>
+                        </React.Fragment>
+                    ))}
+                </div>
+            </div>  
+            </div>      
+        </>
     )
 }
 

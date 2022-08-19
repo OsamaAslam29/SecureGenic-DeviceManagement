2245b067e3360c7bfd6ffb88411b0df7cc75e28d
unknown <osamaaslam029@gmail.com>
2022-08-19 18:15:32 +0500

    style buttons

--- securegenic/src/Pages/Group/Group.jsx
+++ securegenic/src/Pages/Group/Group.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Group.scss'
+import { FaPlus } from 'react-icons/fa';
 import Heading from '../../Components/0-Atoms/Heading/Heading'
 
 const Group = () => {
@@ -7,6 +8,17 @@ const Group = () => {
     <div className='Group_page'>
       <div className="group_container">
         <Heading heading="Group" para="The device group feature allows you to organize your devices into groups."/>
+        <div className="group_button">
+        <button>
+            <div className="icon"><FaPlus /> </div>
+            <div className="text">Add Group</div>
+          </button>
+        <button>
+            <div className="icon"><FaPlus /> </div>
+            <div className="text">Push Application Group</div>
+          </button>
+        </div>
+
       </div>
     </div>
   )

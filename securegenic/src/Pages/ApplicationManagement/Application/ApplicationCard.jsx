6aeb4bd48c17668a2db6acd6ebef3de59434a1da
unknown <osamaaslam029@gmail.com>
2022-08-19 15:58:28 +0500

    wifi added

--- securegenic/src/Pages/ApplicationManagement/Application/ApplicationCard.jsx
+++ securegenic/src/Pages/ApplicationManagement/Application/ApplicationCard.jsx
@@ -7,15 +7,18 @@ import Heading from '../../../Components/0-Atoms/Heading/Heading'
 const ApplicationCard = () => {
   return (
     <div className='application_card'>
-        <Heading heading="Application Management" para="Manage your mobile application on your devices."/>
+      <div className="application_container">
+        <Heading heading="Application Management" para="Manage your mobile application on your devices." />
         <div className="application_button">
-          <div>
-            <button>
-                <div className="icon"><FaPlus/> </div>
-                <div className="text">Add Application</div>
-            </button>
-            </div>
+
+          <button>
+            <div className="icon"><FaPlus /> </div>
+            <div className="text">Add Application</div>
+          </button>
+
         </div>
+      </div>
+
     </div>
   )
 }
--- securegenic/src/Pages/DeviceHealthReport/DeviceHeath.jsx
+++ securegenic/src/Pages/DeviceHealthReport/DeviceHeath.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import './DeviceHealth.scss'
 
 const DeviceHealth = () => {
   return (
     <div>
-        this is device health report component
+        
     </div>
   )
 }

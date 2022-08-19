35233aaa9a292fa2d46f12e0e78eaab752f3a923
unknown <osamaaslam029@gmail.com>
2022-08-19 14:46:04 +0500

    home page changes

--- securegenic/src/Pages/Home/Home.jsx
+++ securegenic/src/Pages/Home/Home.jsx
@@ -35,9 +35,7 @@ const Home = () => {
                 <Route path="/Reports" component={Reports} />
                 <Route path="/PrivacyPolicy" component={Reports} />
         </div>
-        
-        
-
+                
     </>
   )
 }   

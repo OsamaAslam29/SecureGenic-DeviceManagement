8d08d3ae129a594ce0483f592d2b11135223f1c7
JunaidLatif <junaidlatif004@gmail.com>
2022-08-22 21:59:44 +0500

    Seprate Scroll Added

--- securegenic/src/Components/AsideBar/Aside.jsx
+++ securegenic/src/Components/AsideBar/Aside.jsx
@@ -18,23 +18,21 @@ import apply from '../../Assets/Navbar/application_management.png'
 // ********** Body of Application component starts***********
 const Aside = () => {
     return (
-// ****************** Aside Bar code ****************
-        <div className='Aside_component'>
-            <div className="sidenav">
-                <div className="n_icon"><img src={dash} alt="" /><Link to="/" >Dashboard</Link></div>
-                <div className="n_icon"><img src={deep} alt="" /><Link to="/DeepDive" >DeepDive</Link></div>
-                <div className="n_icon"><img src={enroll} alt="" /><Link to="/EnrollDevices" >Enroll Devices</Link></div>
-                <div className="n_icon"><img src={devices} alt="" /><Link to="/Device" >Devices </Link></div>
-                <div className="n_icon"><img src={apply} alt="" /><Link to="/Application" > Application Management</Link></div>
-                <div className="n_icon"><img src={wifi} alt="" /><Link to="/Wifi" >Wifi Profile</Link></div>
-                <div className="n_icon"><img src={groups} alt="" /><Link to="/Group" >Group</Link></div>
-                <div className="n_icon"><img src={locataion} alt="" /><Link to="/Location" >Location</Link></div>
-                <div className="n_icon"><img src={threshold} alt="" /><Link to="/Utilities" >Utilities</Link></div>
-                <div className="n_icon"><img src={administration} alt="" /><Link to="/Administration" >Administration</Link></div>
-                <div className="n_icon"><img src={devicehealth} alt="" /><Link to="/DeviceHealth" >Device Health Report</Link></div>
-                <div className="n_icon"><img src={battery} alt="" /><Link to="/Reports" > Reports</Link></div>
-                <div className="n_icon"><img src={privacy} alt="" /><Link to="/PrivacyPolicy" >Privacy Policy</Link></div>
-            </div>
+        // ****************** Aside Bar code ****************
+        <div className="sidenav">
+            <div className="n_icon"><img src={dash} alt="" /><Link to="/" >Dashboard</Link></div>
+            <div className="n_icon"><img src={deep} alt="" /><Link to="/DeepDive" >DeepDive</Link></div>
+            <div className="n_icon"><img src={enroll} alt="" /><Link to="/EnrollDevices" >Enroll Devices</Link></div>
+            <div className="n_icon"><img src={devices} alt="" /><Link to="/Device" >Devices </Link></div>
+            <div className="n_icon"><img src={apply} alt="" /><Link to="/Application" > Application Management</Link></div>
+            <div className="n_icon"><img src={wifi} alt="" /><Link to="/Wifi" >Wifi Profile</Link></div>
+            <div className="n_icon"><img src={groups} alt="" /><Link to="/Group" >Group</Link></div>
+            <div className="n_icon"><img src={locataion} alt="" /><Link to="/Location" >Location</Link></div>
+            <div className="n_icon"><img src={threshold} alt="" /><Link to="/Utilities" >Utilities</Link></div>
+            <div className="n_icon"><img src={administration} alt="" /><Link to="/Administration" >Administration</Link></div>
+            <div className="n_icon"><img src={devicehealth} alt="" /><Link to="/DeviceHealth" >Device Health Report</Link></div>
+            <div className="n_icon"><img src={battery} alt="" /><Link to="/Reports" > Reports</Link></div>
+            <div className="n_icon"><img src={privacy} alt="" /><Link to="/PrivacyPolicy" >Privacy Policy</Link></div>
         </div>
     )
 }

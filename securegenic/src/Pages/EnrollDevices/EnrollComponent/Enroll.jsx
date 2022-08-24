55fa4b3ff752d6d7a937d8ae7e4c1746f0cda01a
unknown <osamaaslam029@gmail.com>
2022-08-24 16:53:58 +0500

    map add into enroll oage

--- securegenic/src/Pages/EnrollDevices/EnrollComponent/Enroll.jsx
+++ securegenic/src/Pages/EnrollDevices/EnrollComponent/Enroll.jsx
@@ -10,6 +10,42 @@ import qr from '../../../Assets/Enroll/qr.jpg'
 import android from '../../../Assets/Enroll/android.png'
 import Heading from '../../../Components/0-Atoms/Heading/Heading'
 
+const enrollDevice = [
+    {
+        heading: "Step 1: Prepare Device",
+        img: a,
+        para: "Turn on your device. You should see Blue screen asking for language selection (if not then please \n factory reset device).",
+        para2: "See instruction here"
+
+    },
+    {
+        heading: "Step 2: Install Securegenic",
+        img: b,
+        para: "Tap 5 times on screen and it will open up rear camera. Scan this QR Code and connect WiFi on next \n screen. Wait for the App to be installed as it will take some time."
+
+    },
+    {
+        heading: "Step 3: Complete Setup",
+        img: c,
+        para: "Once done it will ask for default launcher selection. Please choose “Securegenic” and press always \n then restart the device."
+
+    },
+      
+    {
+        heading: " Step 4: Verify Setup",
+        img: d,
+        para: "Tap three dots on top right and press info and see if you get Device ID"
+
+    },
+    {
+        heading: "Step 5: Start Management",
+        img: e,
+        para: "Now, Login to Web admin console here or contact your IT department for further steps"
+
+    }
+    
+]
+
 const Enroll = () => {
     return (
         <div className='Enroll_Component'>
@@ -21,7 +57,24 @@ const Enroll = () => {
             <div className="border"></div>
             <div className="enrol_devices">
                 <div className="flex">
-                    <div className="e_devices">
+                    {
+                        enrollDevice.map((enroll) => {
+                            return (
+                                <div className="e_devices">
+                                    <div className="image">
+                                        <img src={enroll.img} alt="" />
+                                    </div>
+                                    <GoPrimitiveDot className='size' />
+                                    <div className="e_heading">
+                                        <div className="heading">{enroll.heading}</div>
+                                        <div className="para"> {enroll.para} </div>
+                                        <div className="para"> {enroll.para2} </div>
+                                    </div>
+                                </div>
+                            )
+                        })
+                    }
+                    {/* <div className="e_devices">
                         <div className="image">
                             <img src={a} alt="" />
                         </div>
@@ -63,7 +116,7 @@ const Enroll = () => {
                             <div className="heading">Step 5: Start Management</div>
                             <div className="para">Now, Login to Web admin console here or contact your IT department for further steps</div>
                         </div>
-                    </div>
+                    </div> */}
                 </div>
                 <div className="e_qr">
                     <img src={qr} alt="" />

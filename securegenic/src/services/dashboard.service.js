241fb3ded9e67355327f8ae14f068b2783a43164
unknown <osamaaslam029@gmail.com>
2022-08-29 17:26:13 +0500

    secure

--- securegenic/src/services/dashboard.service.js
+++ securegenic/src/services/dashboard.service.js
@@ -7,14 +7,24 @@ const dashboard = {
   getDashboardStats: async () => {
     setJWT();
 
-    const [success, error] = await Promisable.asPromise(
-      http.get(`${url}?plant_id=161&time=Daily&date=2022-08-01&type=4`)
-    );
-    if (success) {
-      console.log(success);
-    }
+    fetch("https://app.bel-energise.com/api/login", {
+      body: "{\"email\":\"zara.shah@viiontech.com\", \"password\":{\"design_token1\":\"Hi\",\"design_token2\":\"Hello\",\"design_token3\":\"World\",\"subject_token1\":\"XYZ\"}, \"recipient\":\"james@sample.com\"}",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Token: "sfg999666t673t7t82"
+      },
+      method: "POST"
+    })
 
-    return [success, error];
+    // const [success, error] = await Promisable.asPromise(
+    //   http.get(`${url}?plant_id=161&time=Daily&date=2022-08-01&type=4`)
+    // );
+    // if (success) {
+    //   console.log(success);
+    // }
+
+    // return [success, error];
   },
 };
 
